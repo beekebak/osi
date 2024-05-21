ebcbcb2f86945d7a461e34510ddcc2f45b5ca9d7
beekebak <ribehadance@gmail.com>
2024-05-22 00:51:36 +0700

    fixed copy errors

--- lab6/lab6_3/lab.c
+++ lab6/lab6_3/lab.c
@@ -20,52 +20,50 @@ void PrintErrorAndClose(char* msg, int fd){
   if(close(fd) == -1) perror("Close error");
 }
 
-int ParentFunc(int fd){
+void ParentFunc(int fd){
   int data_socket = accept(fd, NULL, NULL);
   if(data_socket == -1){
     PrintErrorAndClose("Couldnt create socket", fd);
     WaitChild();
-    return 1;
+    return;
   }
   for(unsigned int index = 0; index < UINT_MAX; index++){
     unsigned int value;
     if(read(data_socket, &value, sizeof(unsigned int)) == -1){
       perror("Read error");
       WaitChild();
-      return 1;
+      return;
     }
     if(index != value){
       printf("Sequence error excpected %u got %u\n", index, value);
     }
   }
   WaitChild();
-  return 0;
 }
 
 
-void ChildFunc(int fd){
+void ChildFunc(int fd, struct sockaddr_un addr){
   if(close(fd) == -1) perror("Close error");
   struct sockaddr_un con_addr;
   int con_socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
   if(con_socket_fd == -1){
     perror("Couldnt create socket");
-    return 1;
+    return;
   }
   memset(&con_addr, 0, sizeof(struct sockaddr_un));
   con_addr.sun_family = AF_UNIX;
   strncpy(con_addr.sun_path, SOCKET_PATH, sizeof(con_addr.sun_path) - 1);
-  ret = connect(con_socket_fd, (struct sockaddr*)&con_addr, sizeof(addr));
+  int ret = connect(con_socket_fd, (struct sockaddr*)&con_addr, sizeof(addr));
   if(ret == -1){
     PrintErrorAndClose("Connection error", con_socket_fd);
-    return 1;
+    return;
   }
   for(unsigned int value = 0; value < UINT_MAX; value++){
     if(write(con_socket_fd, &value, sizeof(unsigned int)) == -1){
       PrintErrorAndClose("Write error", con_socket_fd);
-      return 1;
+      return;
     }
   }
-  return 0;
 }
 
 
@@ -82,25 +80,26 @@ int main(){
   strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
   int ret = bind(fd, (struct sockaddr*)&addr, sizeof(addr));
   if(ret == -1){
-    PrintErrorAndClose("Bind error", con_socket_fd);
+    PrintErrorAndClose("Bind error", fd);
     return 1;
   }
   ret = listen(fd, 5);
   if(ret == -1){
-    PrintErrorAndClose("Listen error", con_socket_fd);
+    PrintErrorAndClose("Listen error", fd);
     return 1;
   }
 
   pid_t pid = fork();
   if(pid == -1){
-    PrintErrorAndClose("Fork failure", con_socket_fd);
+    PrintErrorAndClose("Fork failure", fd);
     return 1;
   }
   else if(pid == 0){
-    ChildFunc(fd);
+    ChildFunc(fd, addr);
   }
-  else {
-    if(ParentFunc == 1) break;
+  else{
+    ParentFunc(fd);
+    if(close(fd) == -1) perror("Close error");
   }
   return 0;
 }
